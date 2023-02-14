a458c1b46200146937869bfd91a2dd40092d9f32
EniyavanThangaraj07 <eniyavan.thangaraj@gmail.com>
2023-02-14 12:59:51 +0530

    project structure

---
+++ src/test/java/org/project/NewClass.java
@@ -0,0 +1,23 @@
+package org.project;
+
+public class NewClass {
+
+	public static void main(String[] args) {
+		
+	
+	System.out.println("123");
+	System.out.println("456");
+	System.out.println("789");
+	
+	}
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+}
